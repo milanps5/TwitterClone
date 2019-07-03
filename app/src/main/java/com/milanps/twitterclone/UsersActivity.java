bbff4d462074250b0c359cd514f8889024494225
milan <milanps92@gmail.com>
2019-07-03 10:15:25 +0200

    tweets and feed

--- app/src/main/java/com/milanps/twitterclone/UsersActivity.java
+++ app/src/main/java/com/milanps/twitterclone/UsersActivity.java
@@ -1,19 +1,29 @@
 package com.milanps.twitterclone;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckedTextView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,13 +34,67 @@ public class UsersActivity extends AppCompatActivity {
     ArrayAdapter adapter;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.tweet_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.tweet) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Send a Tweet");
+            final EditText tweetEditText = new EditText(this);
+            builder.setView(tweetEditText);
+            builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.i("Info", tweetEditText.getText().toString());
+
+                    ParseObject tweet = new ParseObject("Tweet");
+                    tweet.put("tweet", tweetEditText.getText().toString());
+                    tweet.put("username", ParseUser.getCurrentUser().getUsername());
+                    tweet.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Toast.makeText(UsersActivity.this, "Tweet sent!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(UsersActivity.this, "Tweet Failed :(", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.i("Info", "I don't wanna tweet!");
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+        } else if (item.getItemId() == R.id.logout) {
+            ParseUser.logOut();
+
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        } else if (item.getItemId() == R.id.viewFeed) {
+            Intent intent = new Intent(getApplicationContext(), FeedActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
 
         setTitle("User List");
 
-        final ListView listView = findViewById(R.id.listView);
+        final ListView listView = findViewById(R.id.feedListView);
         listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
 
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, users);
@@ -54,7 +118,6 @@ public class UsersActivity extends AppCompatActivity {
                 ParseUser.getCurrentUser().saveInBackground();
             }
         });
-
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
         query.findInBackground(new FindCallback<ParseUser>() {
